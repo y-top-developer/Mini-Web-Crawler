789287dbb594618e1b06d762e290ef8b07fd8060
Anton Shumakov <47758828+anton-shumakov@users.noreply.github.com>
2021-05-25 19:56:26 +0300

    Add project

---
+++ MiniWebCrawler/MiniWebCrawler/MiniWebCrawler.fs
@@ -0,0 +1,53 @@
+﻿open System
+open System.IO
+open System.Net
+open System.Text.RegularExpressions
+
+module MiniWebCrawler = 
+    let regexURLs = Regex("<a href\s*=\s*\"?(https?://[^\"]+)\"?\s*>", RegexOptions.Compiled)
+    let regexBadChars = Regex(@"[^\w\.@-]", RegexOptions.Compiled)
+
+    let getAllURLsFrom text =
+        regexURLs.Matches(text)
+        |> Seq.map (fun data  -> data.Groups.[1].Value)
+        |> Seq.filter (fun url -> url <> "")
+
+    let getPageByURL (url:string) =
+        async {
+            try
+                let request = WebRequest.Create url
+                use! respone = request.AsyncGetResponse()
+                use stream = respone.GetResponseStream()
+                use reader = new StreamReader(stream)
+                return Some (url, reader.ReadToEnd())
+            with 
+                | _ -> return None
+        }
+
+    let downloadPages initURL = 
+        let initPage = initURL |> getPageByURL |> Async.RunSynchronously
+        match initPage with
+        | Some (_, data) -> getAllURLsFrom data 
+                            |> Seq.map (fun subURL -> getPageByURL subURL) 
+                            |> Async.Parallel 
+                            |> Async.RunSynchronously
+                            |> Seq.append [initPage]
+        | None -> Seq.empty
+
+    let processPage url (data:string) = 
+        printf $"%A{url} --- %d{data.Length}\n"
+        File.WriteAllText(regexBadChars.Replace(url, "_") + ".txt", data)
+
+    let savePage data = 
+        match data with
+        | Some(url, page) -> processPage url page
+
+    let savePages initURL = 
+        downloadPages initURL |> Seq.map(fun (x:(string * string) option) -> savePage x) |> List.ofSeq |> ignore
+
+[<EntryPoint>]
+let main _ =
+    printf "Enter link:"
+    let request = Console.ReadLine()
+    request |> MiniWebCrawler.savePages
+    0
---
+++ README.md
@@ -0,0 +1,20 @@
+# Mini Web Crawler 🕷 
+This is a tool to download the page and all linked pages
+
+## Usage:
+
+```powershell
+powershell> MiniWebCrawler.exe
+
+Enter link:https://google.com
+"https://google.com" --- 51966
+"http://www.google.ru/intl/ru/services/" --- 75846
+
+powershell> ls
+
+Length Name
+------ ----
+51966 https___google.com.txt
+80004 http___www.google.ru_intl_ru_services_.txt
+```
+
